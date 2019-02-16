064a2af0d3567303da41aee7c422bdb97f9f2e26
Dominik Rowicki <dominik.rowicki@netguru.co>
2019-02-17 00:11:39 +0100

    Create the Sticky component

--- src/index.js
+++ src/index.js
@@ -1,22 +1,83 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React, { Component, createRef, Fragment } from 'react';
+import PropTypes from 'prop-types';
+import cx from 'classnames';
 
-import styles from './styles.css'
+import styles from './styles.css';
 
-export default class ExampleComponent extends Component {
-  static propTypes = {
-    text: PropTypes.string
+class Sticky extends Component {
+  state = { isStuck: false };
+
+  sentinel = createRef();
+
+  sticky = createRef();
+
+  componentDidMount() {
+    this.observer = new IntersectionObserver(this.observerCallback);
+    this.observer.observe(this.sentinel.current);
+    this.sentinelStyle = this.getSentinelStyle();
   }
+
+  componentWillUnmount() {
+    this.observer.disconnect();
+  }
+
+  observerCallback = (entries) => {
+    entries.forEach((entry) => {
+      const targetInfo = entry.boundingClientRect;
+      const rootBoundsInfo = entry.rootBounds;
+
+      if (targetInfo.bottom < rootBoundsInfo.top) {
+        this.setState({ isStuck: true });
+      }
+
+      if (
+        targetInfo.bottom >= rootBoundsInfo.top
+        && targetInfo.bottom < rootBoundsInfo.bottom
+      ) {
+        this.setState({ isStuck: false });
+      }
+    });
+  };
+
+  getSentinelStyle = () => {
+    const stickyStyles = getComputedStyle(this.sticky.current);
+    const top = parseInt(stickyStyles.top, 10);
+
+    return { top: `${-top}px` };
+  };
 
   render() {
-    const {
-      text
-    } = this.props
+    const { children, className, stuckClassName, ...props } = this.props;
+    const { isStuck } = this.state;
 
     return (
-      <div className={styles.test}>
-        Example Component: {text}
-      </div>
-    )
+      <Fragment>
+        <div
+          ref={this.sentinel}
+          className={styles.sentinel}
+          style={this.sentinelStyle}
+        />
+        <div
+          ref={this.sticky}
+          className={cx(styles.sticky, className, { [stuckClassName]: isStuck })}
+          {...props}
+        >
+          {children instanceof Function ? children({ isStuck }) : children}
+        </div>
+      </Fragment>
+    );
   }
 }
+
+Sticky.propTypes = {
+  children: PropTypes.oneOfType([PropTypes.func, PropTypes.node]).isRequired,
+  className: PropTypes.string,
+  stuckClassName: PropTypes.string,
+};
+
+Sticky.defaultProps = {
+  className: '',
+  stuckClassName: '',
+};
+
+export default Sticky;

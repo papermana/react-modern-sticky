99a59a3faea555d6a41cfb22c1cf28547d90687f
Dominik Rowicki <dominik.rowicki@netguru.co>
2019-03-05 14:51:11 +0100

    Add Cypress for testing

---
+++ cypress/server/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import './index.css';
+
+const App = () => (
+  <Router>
+    <div />
+  </Router>
+);
+
+ReactDOM.render(<App />, document.getElementById('root'));

b8243fe632fcd69018ad2a89f3312b763b7feae7
Dominik Rowicki <dominik.rowicki@netguru.co>
2019-03-06 17:44:25 +0100

    Prefix classes related to testing offsets

--- cypress/server/src/Offset.js
+++ cypress/server/src/Offset.js
@@ -3,9 +3,9 @@ import Sticky from 'react-modern-sticky';
 
 const Offset = () => (
   <div className="wrapper">
-    <div className="header" />
-    <div className="inner-wrapper">
-      <Sticky className="sticky-classname sticky-classname--with-offset">
+    <div className="offset__header" />
+    <div className="offset__inner-wrapper">
+      <Sticky className="offset__sticky-classname">
         {({ isStuck }) => (isStuck ? 'is stuck' : 'is not stuck')}
       </Sticky>
     </div>
--- cypress/server/src/OffsetViaProp.js
+++ cypress/server/src/OffsetViaProp.js
@@ -3,8 +3,8 @@ import Sticky from 'react-modern-sticky';
 
 const OffsetViaProp = () => (
   <div className="wrapper">
-    <div className="header" />
-    <div className="inner-wrapper">
+    <div className="offset__header" />
+    <div className="offset__inner-wrapper">
       <Sticky offset={50}>
         {({ isStuck }) => (isStuck ? 'is stuck' : 'is not stuck')}
       </Sticky>

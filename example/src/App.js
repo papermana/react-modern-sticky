d40f514eab63bc5a150e0bacd670cbf8633ada5d
Dominik Rowicki <dominik.rowicki@netguru.co>
2019-03-07 11:39:06 +0100

    In example app, use onStuck events to control header shadow

--- example/src/App.js
+++ example/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Sticky from 'react-modern-sticky';
 import cx from 'classnames';
 
@@ -6,34 +6,52 @@ import usersGroupedByName from './usersGroupedByName';
 import List from './List';
 import Avatar from './Avatar';
 
-export default () => (
-  <div>
-    <Sticky className="header-wrapper">
-      {({ isStuck }) => (
-        <h1 className={cx('header', isStuck && 'header--stuck')}>
-Contacts
-        </h1>
-      )}
-    </Sticky>
-    <div className="header-shadow" />
-    {Object.entries(usersGroupedByName)
-      .sort()
-      .map(([firstLetterOfName, users]) => (
-        <List
-          key={firstLetterOfName}
-          header={firstLetterOfName}
-          values={users}
-          getKey={user => user.name}
-        >
-          {user => (
-            <div className="user">
-              <Avatar gender={user.gender} />
-              <span className="name">
-                {user.name}
-              </span>
-            </div>
-          )}
-        </List>
-      ))}
-  </div>
-);
+export default () => {
+  const [lastSubheaderStuck, setLastSubheaderStuck] = useState(null);
+
+  const handleStuck = (subheader, isStuck) => {
+    if (lastSubheaderStuck === subheader && !isStuck) {
+      setLastSubheaderStuck(null);
+    } else if (!lastSubheaderStuck && isStuck) {
+      setLastSubheaderStuck(subheader);
+    }
+  };
+
+  return (
+    <div>
+      <Sticky className="header-wrapper">
+        {({ isStuck }) => (
+          <h1
+            className={cx(
+              'header',
+              isStuck && 'header--stuck',
+              !lastSubheaderStuck && 'header--has-shadow',
+            )}
+          >
+            Contacts
+          </h1>
+        )}
+      </Sticky>
+      {Object.entries(usersGroupedByName)
+        .sort()
+        .map(([firstLetterOfName, users]) => (
+          <List
+            key={firstLetterOfName}
+            header={firstLetterOfName}
+            values={users}
+            getKey={user => user.name}
+            onStuck={handleStuck}
+          >
+            {user => (
+              <div className="user">
+                <Avatar gender={user.gender} />
+                <span className="name">
+                  {user.name}
+                </span>
+              </div>
+            )}
+          </List>
+        ))}
+    </div>
+  );
+};
--- example/src/List.js
+++ example/src/List.js
@@ -2,11 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Sticky from 'react-modern-sticky';
 
-const List = ({ children, getKey, header, values }) => (
+const List = ({ children, getKey, header, onStuck, values }) => (
   <div>
     <Sticky
       className="list-header-wrapper"
       stuckClassName="list-header-wrapper--stuck"
+      onStuck={isStuck => onStuck(header, isStuck)}
     >
       <div className="list-header">
         {header}
@@ -26,6 +27,7 @@ List.propTypes = {
   children: PropTypes.func.isRequired,
   getKey: PropTypes.func.isRequired,
   header: PropTypes.string.isRequired,
+  onStuck: PropTypes.func.isRequired,
   values: PropTypes.arrayOf(PropTypes.any).isRequired,
 };
 
